f84714e8b5f7d0fda1df2ca927711db6524ae986
nishkur <nkuranjekar1@gmail.com>
2018-04-21 13:29:58 +0530

    ds

--- Test1.R
+++ Test1.R
@@ -2,3 +2,15 @@ women
 #git config --global user.email "nkuranjekar1@gmail.com"
 #git config --global user.name "nishkur"
 mtcars
+install.packages('dplyr')
+mean(women$height)
+?mean
+library(dplyr)
+x <- c(0:10, 50)
+x
+xm <- mean(x)
+c(xm, mean(x, trim = 0.10))
+y=0:100
+y
+
+x[c(3,5,7)]
